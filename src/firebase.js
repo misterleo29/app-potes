09de9bc371d5b9c802d63c7b7bf7b2e2a7ae3711
misterleo29 <leosaliou1@gmail.com>
2025-10-19 14:49:35 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyB_tDzo8-qrLHfGl3vGL0J4eYy0NnFevMc",
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "app-potes.firebaseapp.com",
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || "https://app-potes-default-rtdb.firebaseio.com",
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || "https://app-potes-default-rtdb.firebaseio.com/",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "app-potes",
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "app-potes.firebasestorage.app",
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "822737254215",
